5a7f905c87404c9937c04cb93944338956b92be3
Wesley <wesleysatelis@gmail.com>
2021-06-02 21:59:22 -0300

    dados

---
+++ trabalho_final.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+flights <- read_csv("../trabalho_final/banco/flights.csv")
+airlines <- read_csv("../trabalho_final/banco/airlines.csv")
+airports <- read_csv("../trabalho_final/banco/airports.csv")
+
+
+health <- read_csv("~/Desktop/dados-contagem-escolas.txt")
+symp <- read.table("~/Desktop/dados_baependi.csv", sep=";")
