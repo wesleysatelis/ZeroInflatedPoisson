6f79de0c2fa8cdab89df985021063d7d10f63150
Wesley <wesleysatelis@gmail.com>
2021-06-05 18:53:58 -0300

    inicio Rmd

--- trabalho_final.R
+++ trabalho_final.R
@@ -1,9 +1,10 @@
 library(tidyverse)
 
-flights <- read_csv("../trabalho_final/banco/flights.csv")
-airlines <- read_csv("../trabalho_final/banco/airlines.csv")
-airports <- read_csv("../trabalho_final/banco/airports.csv")
+fish <- read_csv("banco/fish.csv")
 
 
-health <- read_csv("~/Desktop/dados-contagem-escolas.txt")
-symp <- read.table("~/Desktop/dados_baependi.csv", sep=";")
+
+
+
+# health <- read_csv("~/Desktop/dados-contagem-escolas.txt")
+# symp <- read.table("~/Desktop/dados_baependi.csv", sep=";")
